cbe3558d7af17232a971653251fda8a8674dd372
Rafael Fernando de Oliveira Teixeira <rafael.fernando@saaegarca.com>
2024-06-18 10:42:13 -0300

    Initial commit

---
+++ javascript/navigation.js
@@ -0,0 +1,12 @@
+function mostrarImagem() {
+    var img = document.querySelector('.link img');
+    img.style.display = 'block';
+  }
+  
+  function ocultarImagem() {
+    var img = document.querySelector('.link img');
+    img.style.display = 'none';
+  }
+  
+  
+  
